f11b4ca837dc53cf014b8751ceaa47f52468da2f
ManuelMezaLope1 <u23216284@utp.edu.pe>
2026-04-21 22:14:56 -0500

    Añadiendo imágenes y arreglando las rutas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, RouterLink, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
